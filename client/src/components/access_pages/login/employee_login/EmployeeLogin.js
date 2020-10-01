ac674d99b62840741e7ab5253cf3e1fe7e7cd7c0
OzunaEddie <immanuelozuna@gmail.com>
2020-09-30 21:54:26 -0400

    Customer Login

--- client/src/components/access_pages/login/employee_login/EmployeeLogin.js
+++ client/src/components/access_pages/login/employee_login/EmployeeLogin.js
@@ -1,12 +1,83 @@
 import React from 'react';
+import {
+  Link
+} from 'react-router-dom'
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
 import './EmployeeLogin.css'
 
-export default class EmployeeLogin extends React.Component {
- 
+export default class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+      errors: [],
+    };
+  }
+
+  // Handle field change
+  handleChange = (input) => (e) => {
+    this.setState({ [input]: e.target.value });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault(); 
+    const { email, password, errors } = this.state;
+    this.setState({ errors: [] });
+    if (email === "") {
+      this.setState(({errors}) => ({
+        errors: errors.concat("Please Enter an Email")
+      }));
+    }
+    if (password === "") {
+      this.setState(({errors}) => ({
+        errors: errors.concat("Please Enter a Password")
+      }));
+    }
+    if(errors.length === 0) {
+      // Insert Backend Here.
+      
+    }
+  };
 
   render() {
     return (
-      <div>Employee Login Page</div>
+     
+        <Container className="container-bg rounded px-5 py-4 mx-4">
+        <h2 className="text-light text-center">Sign in with your ChefItUp account</h2>
+          { this.state.errors.length > 0 ?  
+            this.state.errors.map((error,index) => {
+              return <li key={index} className="text-warning"> {error} </li>
+          })
+          : 
+          <div></div>
+          } 
+          <Form>
+            <Form.Group controlId="formEmail">
+              <Form.Label className="text-light">Email Address:</Form.Label>
+              <Form.Control type="email" placeholder="Enter Email" 
+                onChange={this.handleChange("email")}/>
+            </Form.Group>
+            <Form.Group controlId="formPassword">
+              <Form.Label className="text-light">Password:</Form.Label>
+              <Form.Control type="password" placeholder="Enter Password" 
+                onChange={this.handleChange("password")}/>
+            </Form.Group>
+          </Form>
+          <Button variant="primary" type="submit" size="lg" block
+            onClick={this.handleSubmit}>
+            Sign in
+          </Button>
+          <br/>
+          <p className="text-light">
+            Don't have an account? <Link className="text-primary">Sign Up</Link>
+          </p>  
+          <Link to="ForgotPassword" className="text-primary">Forgot Your Password?</Link>
+        </Container>
+     
     );
   }
 }
