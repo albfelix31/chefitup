92207794c14b73e284a7d8dacc0197f2973bc244
Nahin Imtiaz <nahinimtiaz147@gmail.com>
2020-11-22 14:53:53 -0500

    Fixed menu tab for customer users.

--- client/src/components/navbars/nav_customer/NavCustomer.js
+++ client/src/components/navbars/nav_customer/NavCustomer.js
@@ -18,23 +18,23 @@ export default class NavCustomer extends React.Component {
               <Button className="searchBtn" variant="outline-success">Search</Button>
             </Form>
             <NavDropdown className="ml-auto navItem" title="Hi, Eddie" id="basic-nav-dropdown dropdown-menu-align-right" alignRight>
-                <NavDropdown.Item href="#action/3.1">Past orders</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Payments</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Discussion Forum</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.4">Account</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.5">Help</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.6">Warnings</NavDropdown.Item>
+                <NavDropdown.Item href="./PastOrders">Past orders</NavDropdown.Item>
+                <NavDropdown.Item href="./Payment">Payments</NavDropdown.Item>
+                <NavDropdown.Item href="./Discussion">Discussion Forum</NavDropdown.Item>
+                <NavDropdown.Item href="./Profile">Account</NavDropdown.Item>
+                <NavDropdown.Item href="#">Help</NavDropdown.Item>
+                <NavDropdown.Item href="./WarningsCustomer">Warnings</NavDropdown.Item>
                 <NavDropdown.Divider />
                 <NavDropdown.Item className="navSignOut" href="#action/3.6">Sign out</NavDropdown.Item>
             </NavDropdown>
             </Nav>
 
             <NavDropdown className="ml-auto shopping-menu" title={<Image className="shopping" src={ShoppingBag} fluid/>} id="basic-nav-dropdown dropdown-menu-align-right" alignRight>
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+            <NavDropdown.Item href="./Cart">Shopping Cart</NavDropdown.Item>
+                {/* <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
                 <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item> */}
             </NavDropdown>
         </Navbar.Collapse>
     </Navbar>
