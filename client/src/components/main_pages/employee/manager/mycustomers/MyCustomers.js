1c6fec7860e447ab25f66f828fb09a5e432737eb
Cristian <ccuevas3410@gmail.com>
2020-12-02 12:24:22 -0500

    Added VIP/Regular customers modal, needs css fixing

--- client/src/components/main_pages/employee/manager/mycustomers/MyCustomers.js
+++ client/src/components/main_pages/employee/manager/mycustomers/MyCustomers.js
@@ -18,6 +18,7 @@ import Profile from "../profile.png"
 import Add from "../../img/add.png"
 import Dropdown from "react-bootstrap/Dropdown"
 import Modal from "react-bootstrap/Modal"
+import { Fade } from 'react-bootstrap';
 const CustomToggle = React.forwardRef(({ children, onClick }, ref) => (
   <a
     className="threedots-a"
@@ -38,13 +39,26 @@ export default class MyCustomers extends React.Component {
         // Insert Backend Call For Textbooks When Nothing is on Search
 
     this.setState({
-        employee: [
+        customers: [
           {name: "Yes Sir",compliments: 1, complaints: 2, image: Profile},
           {name: "No sir",compliments: 5, complaints: 0, image: Profile},
           {name: "Joane Tho",compliments: 2, complaints: 5, image: Profile},
           {name: "Kha Zhix",compliments: 10, complaints: 3, image: Profile},
           {name: "Rengar Blizt",compliments: 2, complaints: 2, image: Profile},
+        ],
+
+        applyingCustomers: [
+          {name: "Good",lastName: "Customer", userName: "Customer003", deposit:200, email:"Cgood003@gmail.com"},
+          {name: "Bad",lastName: "Customer", userName: "Bad003", deposit:100, email:"BadC@gmail.com"},
+         
+        ],
+
+        vipCustomers: [
+          {name: "VIP",lastName: "Important", userName: "VIP001", email:"VIPImportnat@gmail.com"},
+          {name: "Not",lastName: "Important", userName: "Not02",  email:"NotVip@gmail.com"},
+         
         ]
+
       });
       }
 
@@ -52,8 +66,12 @@ export default class MyCustomers extends React.Component {
         super(props);
         this.state = {
           search: "",
-          employee: [],
+          customers: [],
+          applyingCustomers:[],
+          vipCustomers:[],
           isOpen: false,
+          isOpenAddCustomer:false,
+          isOpenVIP:false,
           n: null
         };
       }
@@ -62,16 +80,32 @@ export default class MyCustomers extends React.Component {
         e.preventDefault();
         this.setState({ isOpen: true, n: index })
     };
+
+    
+    openModalCustomer = index => (e) => {
+      e.preventDefault();
+      this.setState({ isOpenAddCustomer: true, n: index })
+  };
+
+  openModalVIPRegular = index => (e) => {
+    e.preventDefault();
+    this.setState({ isOpenVIP: true, n: index })
+};
+
+    addEmployee = index => (e) => {
+      e.preventDefault();
+      this.setState({ isOpenAddCustomer: true, n: index })
+  };
     closeModal = () => {
-        this.setState({ isOpen: false })
+        this.setState({ isOpen: false, isOpenAddCustomer: false, isOpenVIP:false })
 
     };
 
 
     saveEmployee = () => {
     
-    
-    console.log(this.state.n)
+
+      //Send information to api
     this.setState({ isOpen: false })
 
     }
@@ -174,21 +208,136 @@ export default class MyCustomers extends React.Component {
       </Modal>
       
       
+{/* Modal to add employees */}
+<Modal show={this.state.isOpenAddCustomer} onHide={this.closeModal}>
+<Modal.Header closeButton>
+  <Modal.Title>Add Customer</Modal.Title>
+</Modal.Header>
+
+
+<Modal.Body>
+{/* 
+  For every customer, make a new form and list group */}
+
+<div className="list">
+  <ListGroup horizontal="lg" className="my-2" >
+    <ListGroup.Item>First Name</ListGroup.Item>
+    <ListGroup.Item>Last Name</ListGroup.Item>
+    <ListGroup.Item>Username</ListGroup.Item>
+    <ListGroup.Item>Deposit</ListGroup.Item>
+    <ListGroup.Item>Email</ListGroup.Item>
+  </ListGroup>
+
+{ 
+          this.state.applyingCustomers.map((list, index) => (
+              
+    <Form>
+    <ListGroup horizontal="lg" className="my-2" >
+    <ListGroup.Item variant="dark">{list.name}</ListGroup.Item>
+    <ListGroup.Item variant="dark">{list.lastName}</ListGroup.Item>
+    <ListGroup.Item variant="dark">{list.userName}</ListGroup.Item>
+    <ListGroup.Item variant="dark">{list.deposit}</ListGroup.Item>
+    <ListGroup.Item variant="dark">{list.email}</ListGroup.Item>
+    <ListGroup.Item variant="dark">  <Button variant="primary" onClick={this.saveEmployee}>
+   Accept
+  </Button></ListGroup.Item>
+    <ListGroup.Item variant="dark">  <Button variant="primary" onClick={this.saveEmployee}>
+   Decline
+  </Button></ListGroup.Item>
+</ListGroup>
+    
+    </Form>
+
+          ))}
+
+</div>
+
+
+</Modal.Body>
+<Modal.Footer>
+  <Button variant="primary" onClick={this.saveEmployee}>
+    Save
+  </Button>
+  <Button variant="primary" onClick={this.closeModal}>
+    Close
+  </Button>
+</Modal.Footer>
+</Modal>
       
+
+
       
+{/* Modal to see regular/VIP customers   */}
+<Modal show={this.state.isOpenVIP} onHide={this.closeModal}>
+<Modal.Header closeButton>
+  <Modal.Title> {this.state.n == 'vip' ? "⭐ VIP Customer" : "Regular Customers"}</Modal.Title>
+</Modal.Header>
+
+<Modal.Body>
+{/* 
+  For every customer, make a new form and list group */}
+
+<div className="list">
+  <ListGroup horizontal="lg" className="my-2" >
+    <ListGroup.Item>First Name</ListGroup.Item>
+    <ListGroup.Item>Last Name</ListGroup.Item>
+    <ListGroup.Item>Username</ListGroup.Item>
+    <ListGroup.Item>Deposit</ListGroup.Item>
+    <ListGroup.Item>Email</ListGroup.Item>
+  </ListGroup>
+
+{ 
+          this.state.applyingCustomers.map((list, index) => (
+              
+    <Form>
+    <ListGroup horizontal="lg" className="my-2" >
+    <ListGroup.Item variant="dark">{list.name}</ListGroup.Item>
+    <ListGroup.Item variant="dark">{list.lastName}</ListGroup.Item>
+    <ListGroup.Item variant="dark">{list.userName}</ListGroup.Item>
+    <ListGroup.Item variant="dark">{list.deposit}</ListGroup.Item>
+    <ListGroup.Item variant="dark">{list.email}</ListGroup.Item>
+    <ListGroup.Item variant="dark">  <Button variant="primary" onClick={this.saveEmployee}>
+   Accept
+  </Button></ListGroup.Item>
+    <ListGroup.Item variant="dark">  <Button variant="primary" onClick={this.saveEmployee}>
+   Decline
+  </Button></ListGroup.Item>
+</ListGroup>
+    
+    </Form>
+
+          ))}
+
+</div>
+
+
+</Modal.Body>
+<Modal.Footer>
+  <Button variant="primary" onClick={this.saveEmployee}>
+    Save
+  </Button>
+  <Button variant="primary" onClick={this.closeModal}>
+    Close
+  </Button>
+</Modal.Footer>
+</Modal>
+      
+
+
+
+
       <Row className="row-top">
           <Col>
               <h1>My Customers</h1>  
           </Col>
           <Col>
             <div className="btn-container">
-              <Button className="btn-employee" onClick={this.openModal("add")} variant="success"><Image className="add-sign" src={Add}/>Add Customers</Button>
-
-             <Button className="btn-employee" variant="primary">
+              <Button className="btn-employee" onClick={this.openModalCustomer()} variant="success"><Image className="add-sign" src={Add}/>Add Customers</Button>
+             <Button className="btn-employee" onClick={this.openModalVIPRegular("vip")}  variant="primary">
              ⭐ VIP <Badge variant="light">2</Badge>
               </Button>
              
-              <Button className="btn-employee" variant="primary">
+              <Button onClick={this.openModalVIPRegular()} className="btn-employee" variant="primary">
                 Regular <Badge variant="light">2</Badge>
               </Button>
               <Button className="btn-employee" variant="primary">
@@ -222,7 +371,7 @@ export default class MyCustomers extends React.Component {
       </Form>
       <Row className="row-resize">
         { 
-          this.state.employee.map((list, index) => (
+          this.state.customers.map((list, index) => (
               
             <Col key={index} sm="6" md="4" lg="3" className="book-selection">           
               <Card className="text-center employees">
