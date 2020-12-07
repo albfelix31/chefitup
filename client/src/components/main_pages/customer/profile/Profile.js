a12faf7304fe25ea125bb49a808a7efb0730443a
Nahin Imtiaz <nimtiaz000@citymail.cuny.edu>
2020-12-07 01:26:45 -0500

    Modified Profile.js API calls.

--- client/src/components/main_pages/customer/profile/Profile.js
+++ client/src/components/main_pages/customer/profile/Profile.js
@@ -1,29 +1,55 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
 import './Profile.css'
 
 import {Container, Form, Button, Modal, Image} from 'react-bootstrap'
 import Edit from "./edit.svg"
-
+import jwt_decode from "jwt-decode";
+import Cookies from 'universal-cookie';
 
 const Profile = () => {
 
-  const [profile,setProfile] = useState({name:'Joe Biden',email:'jbiden000@citymail.cuny.edu',address:'1600 Pennsylvania Avenue NW, Washington, DC 20500',paymentMethod:'************-1234',balance:'150.00',subscription: true});
+  const [profile,setProfile] = useState({});
   const [isOpen,setIsOpen]= useState(false)
   const [edit,setEdit] = useState({editName:'',editValue:''})
+
+  //Fetching profile data and setting it to profile state
+  useEffect( ()=>{
+    // Insert Backend Call For dishes 
+    const API = new api();
+    const cookies = new Cookies();
+    //const userID 
+    const userID = jwt_decode(cookies.get('token')).userId;
+    let profile = {}
+    //Fetching rating for each dish
+    API.getProfile(userID).then ( data => {
+      profile = {
+        name: data['name'],
+        email: data['email'],
+        address: data['address'],
+        payment: data['payment'],
+        balance: data['balance'],
+        subscribe: data['subscribe']
+      }
+    })
+    setProfile(profile);
+  },[]);
 
   const handleChange = (e) => {
     const newValue = e.target.value
     setEdit({...edit,editValue:newValue})
   }
-  // This will be needed in confirm button to update the changes in API
-  // const handleSubmit = (e) => {
-  //   e.preventDefault()
-  //   if(payment.cardName && payment.cardNumber && payment.expMonth && payment.expYear && payment.cvv && payment.depositAmount){
-  //     const newData = {...payment}
-  //     setPaymentData([...paymentData,newData])
-  //     setPayment({cardName:'',cardNumber:'',expMonth:'',expYear:'',cvv:'',depositAmount:''})
-  //   }
-  // }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if(profile.name && profile.email && profile.address && profile.payment && profile.balance){
+      const API = new api();
+      const data = {...profile}
+      API.editProfile(data).then( error => {
+        console.log(error);
+      })
+    }
+    window.location.href='/Menu';
+  }
   
   const openModal = (e) => {
     e.preventDefault();
@@ -38,7 +64,7 @@ const Profile = () => {
   };
   const saveEdit = () => {
     if(edit.editName==="subscription"){
-      setProfile({...profile,subscription:!profile.subscription})
+      setProfile({...profile,subscribe:!profile.subscribe})
       setIsOpen(false)
     }
     else{
@@ -72,7 +98,7 @@ const Profile = () => {
       :
       <Modal show={isOpen} onHide={closeModal}>
         <Modal.Header closeButton>
-            <Modal.Title>Please Confirm Your {profile.subscription ? 'Unsubscription' : 'Subscription'} !</Modal.Title>
+            <Modal.Title>Please Confirm Your {profile.subscribe ? 'Unsubscription' : 'Subscription'} !</Modal.Title>
         </Modal.Header>
         <Modal.Footer >
           <Button className="btn-edit-modal" variant="primary" onClick={saveEdit}>
@@ -95,16 +121,16 @@ const Profile = () => {
       <p><strong>Address: {profile.address}</strong>
       <Button className="btn-edit" variant="light" name="address" value={profile.address} onClick={openModal}><Image className="edit-sign" src={Edit}/> Edit</Button></p>
       <hr/>
-      <p><strong>Payment Method: {profile.paymentMethod}</strong>
-      <Button className="btn-edit" variant="light" name="paymentMethod" value={profile.paymentMethod} onClick={openModal}><Image className="edit-sign" src={Edit}/> Edit</Button></p>
+      <p><strong>Payment Method: {profile.payment}</strong>
+      <Button className="btn-edit" variant="light" name="paymentMethod" value={profile.payment} onClick={openModal}><Image className="edit-sign" src={Edit}/> Edit</Button></p>
       <hr/>
       <p><strong>Balance: {'\u0024'}{profile.balance}</strong>
       <Button className="btn-edit" variant="light" name="balance" value={profile.balance} onClick={openModal}><Image className="edit-sign" src={Edit}/> Edit</Button></p>
       <hr/>
-      <p><strong>Unsubscribe Membership</strong>
-      <Button className="btn-edit" variant="light" name="subscription" value={profile.subscription} onClick={openModal}>{profile.subscription ? 'Unsubscribe' : 'Subscribe'}</Button></p>
+      <p><strong>Membership</strong>
+      <Button className="btn-edit" variant="light" name="subscription" value={profile.subscribe} onClick={openModal}>{profile.subscribe ? 'Unsubscribe' : 'Subscribe'}</Button></p>
       <hr/>
-      <Button className="btn-profile" variant="primary" href="/Menu">
+      <Button className="btn-profile" variant="primary" onClick={handleSubmit}>
           confirm
       </Button>
     </Container>
